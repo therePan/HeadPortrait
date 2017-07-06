a6d34c4b9a3d2843d2c65afa7e905ebc5f5c9fc8
zsl99a <1033392732@qq.com>
2017-07-06 14:20:12 +0800

    transform-runtime

--- dist/index.js
+++ dist/index.js
@@ -2,6 +2,18 @@
 
 exports.__esModule = true;
 
+var _classCallCheck2 = require('babel-runtime/helpers/classCallCheck');
+
+var _classCallCheck3 = _interopRequireDefault(_classCallCheck2);
+
+var _possibleConstructorReturn2 = require('babel-runtime/helpers/possibleConstructorReturn');
+
+var _possibleConstructorReturn3 = _interopRequireDefault(_possibleConstructorReturn2);
+
+var _inherits2 = require('babel-runtime/helpers/inherits');
+
+var _inherits3 = _interopRequireDefault(_inherits2);
+
 var _class, _temp2;
 
 var _react = require('react');
@@ -18,25 +30,19 @@ var _piexifjs2 = _interopRequireDefault(_piexifjs);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
-
-function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
-
 var headPortrait = (_temp2 = _class = function (_React$Component) {
-  _inherits(headPortrait, _React$Component);
+  (0, _inherits3.default)(headPortrait, _React$Component);
 
   function headPortrait() {
     var _temp, _this, _ret;
 
-    _classCallCheck(this, headPortrait);
+    (0, _classCallCheck3.default)(this, headPortrait);
 
     for (var _len = arguments.length, args = Array(_len), _key = 0; _key < _len; _key++) {
       args[_key] = arguments[_key];
     }
 
-    return _ret = (_temp = (_this = _possibleConstructorReturn(this, _React$Component.call.apply(_React$Component, [this].concat(args))), _this), _this.handleChange = function (ev) {
+    return _ret = (_temp = (_this = (0, _possibleConstructorReturn3.default)(this, _React$Component.call.apply(_React$Component, [this].concat(args))), _this), _this.handleChange = function (ev) {
       ev.target.files[0] && _this.reader.readAsDataURL(ev.target.files[0]);
       ev.target.value = '';
     }, _this.zipImage = function () {
@@ -76,7 +82,7 @@ var headPortrait = (_temp2 = _class = function (_React$Component) {
       });
 
       _this.props.onChange(base64, blob);
-    }, _temp), _possibleConstructorReturn(_this, _ret);
+    }, _temp), (0, _possibleConstructorReturn3.default)(_this, _ret);
   }
 
   headPortrait.prototype.componentWillMount = function componentWillMount() {
